ab7e07038c092c55c671b3be63378fbc3ee05e52
DEEPAKKUSHWAHAGBPIET <51187511+DEEPAKKUSHWAHAGBPIET@users.noreply.github.com>
2025-08-29 10:26:38 +0530

    optimal solution that passed all the test cases using heap

--- problem_295.c
+++ problem_295.c
@@ -1,234 +1,102 @@
-typedef enum {red, black} Color;
-
-typedef struct Node{
-    int value;
-    int size;
-    Color color;
-    struct Node *left, *right, *parent;
-}Node;
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
-    Node *root;
+    int *data;
+    int size;
+    int capacity;
+    int isMinHeap; // 1 for min-heap, 0 for max-heap
+} Heap;
+
+Heap* createHeap(int capacity, int isMinHeap) {
+    Heap* h = (Heap*)malloc(sizeof(Heap));
+    h->data = (int*)malloc(sizeof(int) * capacity);
+    h->size = 0;
+    h->capacity = capacity;
+    h->isMinHeap = isMinHeap;
+    return h;
+}
+
+void swap(int *a, int *b) {
+    int t = *a; *a = *b; *b = t;
+}
+
+int compare(Heap* h, int a, int b) {
+    if (h->isMinHeap) return a < b;   // min-heap
+    else return a > b;                // max-heap
+}
+
+void heapPush(Heap* h, int val) {
+    if (h->size == h->capacity) return; 
+    h->data[h->size] = val;
+    int i = h->size++;
+    while (i > 0) {
+        int p = (i - 1) / 2;
+        if (compare(h, h->data[i], h->data[p])) {
+            swap(&h->data[i], &h->data[p]);
+            i = p;
+        } else break;
+    }
+}
+
+int heapPop(Heap* h) {
+    if (h->size == 0) return 0;
+    int root = h->data[0];
+    h->data[0] = h->data[--h->size];
+    int i = 0;
+    while (1) {
+        int l = 2*i+1, r = 2*i+2, target = i;
+        if (l < h->size && compare(h, h->data[l], h->data[target])) target = l;
+        if (r < h->size && compare(h, h->data[r], h->data[target])) target = r;
+        if (target != i) {
+            swap(&h->data[i], &h->data[target]);
+            i = target;
+        } else break;
+    }
+    return root;
+}
+
+int heapTop(Heap* h) {
+    return h->size ? h->data[0] : 0;
+}
+
+typedef struct {
+    Heap *left;  // max heap
+    Heap *right; // min heap
 } MedianFinder;
 
-Node* createNode(int value){
-    Node *newNode = (Node *)malloc(sizeof(Node));
-    if(newNode==NULL) return NULL;
-
-    newNode->value = value;
-    newNode->size = 1;
-   
-    newNode->color = red;
-    newNode->left = newNode->right = newNode->parent = NULL;
-
-    return newNode;
-}
-
-int getSize(Node *root){
-    return root ? root->size : 0;
-}
-
-void updateSize(Node *root){
-    if(root==NULL) return;
-    root->size = getSize(root->left) + 1 + getSize(root->right);
-}
-
-void leftRotate(MedianFinder *mf, Node *x){
-    if(x==NULL || x->right==NULL) return;
-    Node *y = x->right;
-    x->right = y->left;
-
-    if(y->left!=NULL){
-        y->left->parent = x;
-    }
-
-    if(x->parent==NULL){
-        mf->root = y;
-    }
-    else if(x==x->parent->left){
-        x->parent->left = y;
-    }
-    else{
-        x->parent->right = y;
-    }
-
-    y->left = x;
-    x->parent = y;
-
-    updateSize(x);
-    updateSize(y);
-}
-
-void rightRotate(MedianFinder *mf, Node *x){
-    if(x==NULL || x->left==NULL) return;
-    Node *y = x->left;
-    x->left = y->right;
-
-    if(y->right!=NULL){
-        y->right->parent = x;
-    }
-
-    if(x->parent==NULL){
-        mf->root = y;
-    }
-    else if(x==x->parent->left){
-        x->parent->left = y;
-    }
-    else{
-        x->parent->right = y;
-    }
-
-    y->right = x;
-    x->parent = y;
-
-    updateSize(x);
-    updateSize(y);
-}
-
-void fixInsert(MedianFinder *mf, Node *z){
-    while(z->parent && z->parent->color==red){
-        Node *parent = z->parent;
-        Node *grandParent = parent->parent;
-
-        if(grandParent && parent==grandParent->left){
-            Node *uncle = grandParent->right;
-            if(uncle && uncle->color==red){
-                uncle->color = black;
-                parent->color = black;
-                grandParent->color = red;
-
-                z = grandParent;
-            }
-            else{
-                if(z==parent->right){
-                    z = parent;
-                    leftRotate(mf, z);
-                }
-
-                parent->color = black;
-                grandParent->color = red;
-                rightRotate(mf, grandParent);
-            }
-        }
-        else{
-            Node* uncle = grandParent->left;
-            if(uncle && uncle->color==red){
-                uncle->color = black;
-                parent->color = black;
-                grandParent->color = red;
-
-                z = grandParent;
-            }
-            else{
-                if(z==parent->left){
-                    z = parent;
-                    rightRotate(mf, z);
-                }
-                parent->color = black;
-                grandParent->color = red;
-
-                leftRotate(mf, grandParent);
-            }
-        }
-    }
-    if(mf->root!=NULL)
-     mf->root->color = black;
-}
-
-void insertValue(MedianFinder *mf, int val){
-    Node *prev = NULL;
-    Node *curr = mf->root;
-
-    while(curr){
-        prev = curr;
-        curr->size++;
-        if(val<curr->value){
-            curr = curr->left;
-        }
-        else{
-            curr = curr->right;
-        }
-    }
-
-    Node *z = createNode(val);
-    z->parent = prev;
-    if(prev==NULL){
-        mf->root = z;
-    }
-    if(prev!=NULL){
-        if(val<prev->value){
-            prev->left = z;
-        }
-        else{
-            prev->right = z;
-        }
-    }
-
-    fixInsert(mf, z);
-}
-
-Node *findKthLargest(Node *root, int k){
-    if(root==NULL) return NULL;
-
-    int leftsize = getSize(root->left);
-    if(leftsize+1 == k) return root;
-    if(k<=leftsize) return findKthLargest(root->left, k);
-    else
-        return findKthLargest(root->right, k-leftsize-1);
-}
-
-
 MedianFinder* medianFinderCreate() {
-    MedianFinder *mf = (MedianFinder *)malloc(sizeof(MedianFinder));
-    if(mf==NULL) return NULL;
-
-    mf->root = NULL;
+    MedianFinder* mf = (MedianFinder*)malloc(sizeof(MedianFinder));
+    mf->left = createHeap(100000, 0);  // max heap
+    mf->right = createHeap(100000, 1); // min heap
     return mf;
 }
 
 void medianFinderAddNum(MedianFinder* obj, int num) {
-    insertValue(obj, num);
+    if (obj->left->size == 0 || num <= heapTop(obj->left))
+        heapPush(obj->left, num);
+    else
+        heapPush(obj->right, num);
+
+    // Balance heaps
+    if (obj->left->size > obj->right->size + 1) {
+        heapPush(obj->right, heapPop(obj->left));
+    } else if (obj->right->size > obj->left->size) {
+        heapPush(obj->left, heapPop(obj->right));
+    }
 }
 
 double medianFinderFindMedian(MedianFinder* obj) {
-    int totalsize = getSize(obj->root);
-    if(totalsize%2 == 1){
-        Node *temp = findKthLargest(obj->root, (totalsize+1)/2);
-        return (double)temp->value;
-    }
-    else{
-        Node *n1 = findKthLargest(obj->root, totalsize/2);
-        Node *n2 = findKthLargest(obj->root, totalsize/2 + 1);
-        double n1value = 0;
-        double n2value = 0;
-        if(n1!=NULL){
-            n1value = (double)n1->value;
-        }
-        if(n2!=NULL){
-            n2value = (double)n2->value;
-        }
-        return (n1value+n2value)/2.0;
-    }
-}
-
-void freetree(Node* root){
-    if(root==NULL) return;
-
-    freetree(root->left);
-    freetree(root->right);
+    if (obj->left->size > obj->right->size)
+        return heapTop(obj->left);
+    else
+        return ((double)heapTop(obj->left) + heapTop(obj->right)) / 2.0;
 }
 
 void medianFinderFree(MedianFinder* obj) {
-    freetree(obj->root);
+    free(obj->left->data);
+    free(obj->right->data);
+    free(obj->left);
+    free(obj->right);
     free(obj);
 }
-
-/**
- * Your MedianFinder struct will be instantiated and called as such:
- * MedianFinder* obj = medianFinderCreate();
- * medianFinderAddNum(obj, num);
- 
- * double param_2 = medianFinderFindMedian(obj);
- 
- * medianFinderFree(obj);
-*/
